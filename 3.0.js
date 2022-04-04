20bb24a54ab20b4820d3e3eea12733fe4d1fa189
ObjectifGuitare <sebastien@DESKTOP-5GSLS9M.localdomain>
2022-04-04 15:32:34 +0200

    3.0 done

---
+++ 3.0.js
@@ -0,0 +1,4 @@
+let hopper = ["Gilles", "Jamie", "Jason", "Jérémy", "Martin", "Rayane", "Rayhan", "Ricardo", "Steffanie", "Sébastien", "Tanguy", "Thibaud", "Zakaria"];
+
+for (let elem of hopper)
+    console.log(elem);
