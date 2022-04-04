e27f5f8ac2c3497319026e55c498681f23674a46
ObjectifGuitare <sebastien@DESKTOP-5GSLS9M.localdomain>
2022-04-04 14:53:16 +0200

    2.5 done

---
+++ 2.5.js
@@ -0,0 +1,9 @@
+let fav = new Number(prompt("what's your FAVORITE number ? ( ͡° ͜ʖ ͡°)"));
+while (fav != 42)
+{
+
+    console.log("are you sure ?? (╯ ͠° ͟ʖ ͡°)╯┻━┻");
+
+    fav = new Number(prompt("what's your FAVORITE number ? ( ͠° ͟ʖ ͡°)"));
+
+}
