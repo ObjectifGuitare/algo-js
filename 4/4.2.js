c4ee05741aa0a9ef1c97152958e70f78fa684d07
becode <patez.sebastien@gmail.com>
2022-04-05 14:34:20 +0200

    4.2 done

---
+++ 4/4.2.js
@@ -0,0 +1,7 @@
+// this is documentation for the rand10 function which generate random numbers between 1 and 10
+function rand10()
+{
+    return Math.floor(Math.random() * 10 + 1);
+}
+
+console.log(rand10());
